c0303448ebb7f35cc976fef1ac6a14a0b182dc14
Keno Bacunawa <keno@bacunawa.com>
2023-01-18 16:15:58 +0800

    [BUG-FIX] - SETUP Docker for FE and BE

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  webpackDevMiddleware: (config) => {
+    // Solve compiling problem via vagrant
+    config.watchOptions = {
+      poll: 1000,   // Check for changes every second
+      aggregateTimeout: 300,   // delay before rebuilding
+    };
+    return config;
+  }
+};
