ad5462b4f45990b6ec39af079c2b6bfb780cdb7f
Jhon Rhon Escabillas <114911074+jescabillas@users.noreply.github.com>
2023-01-20 14:58:14 +0800

    [Overflow-173] Setup tailwind (#13)

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  webpackDevMiddleware: (config) => {
+  webpack: (config) => {
     // Solve compiling problem via vagrant
     config.watchOptions = {
       poll: 1000,   // Check for changes every second
