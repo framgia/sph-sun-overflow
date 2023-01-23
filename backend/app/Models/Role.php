03fe80ed6ae5aea334e591b4f3c34cc1005f4b92
jules-lucero <julesrussel.lucero@sun-asterisk.com>
2023-01-23 15:16:42 +0800

    [Overflow-172] Pivot table for Roles and Permissions

--- backend/app/Models/Role.php
+++ backend/app/Models/Role.php
@@ -16,4 +16,9 @@ class Role extends Model
     {
         return $this->hasMany(User::class);
     }
+
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class, 'permission_role');
+    }
 }
