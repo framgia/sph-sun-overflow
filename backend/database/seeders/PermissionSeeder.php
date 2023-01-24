addd37433ab16f53dceb0e50cd967ab4961b3f46
jules-lucero <julesrussel.lucero@sun-asterisk.com>
2023-01-24 12:59:55 +0800

    Populate pivot table

--- backend/database/seeders/PermissionSeeder.php
+++ backend/database/seeders/PermissionSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Permission;
+use App\Models\Role;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -132,5 +133,13 @@ class PermissionSeeder extends Seeder
                 'description' => 'Can delete own team role'
             ],
         ], ['id'], ['name', 'description']);
+
+        $admin = Role::find(1);
+        $teamLead = Role::find(2);
+        $user = Role::find(3);
+
+        $admin->permissions()->sync([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]);
+        $teamLead->permissions()->sync([1, 2, 3, 4, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]);
+        $user->permissions()->sync([1, 2, 3, 4, 8, 10, 11, 12, 13, 14, 15, 16, 17]);
     }
 }
