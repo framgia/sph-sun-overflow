97cc73ee4eac0ce466060ebfd2247e902a1e655c
Jhon Rhon Escabillas <114911074+jescabillas@users.noreply.github.com>
2023-01-24 14:38:57 +0800

    [Overflow-174] Set-up color theme (#14)

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -4,15 +4,18 @@ import { Inter } from '@next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-	return (
-		<>
-			<Head>
-				<title>Sun* Overflow</title>
-        <meta name="description" content="SunOverflow is an internal project for the employees of Sun Asterisk PH that aims to help the team with asking and answering questions." />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
+  return (
+    <>
+      <Head>
+        <title>Sun* Overflow</title>
+        <meta
+          name='description'
+          content='SunOverflow is an internal project for the employees of Sun Asterisk PH that aims to help the team with asking and answering questions.'
+        />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
       <main></main>
-		</>
-	);
+    </>
+  );
 }
