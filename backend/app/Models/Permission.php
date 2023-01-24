fbc291118301100d16d65f05a0cb512fa3ecea6b
jules-lucero <111732984+jules-lucero@users.noreply.github.com>
2023-01-24 15:27:44 +0800

    [Overflow-172] Pivot table for Roles and Permissions (#18)
    * [Overflow-172] Pivot table for Roles and Permissions
    
    * Populate pivot table
    
    * Add down function for dropping permission_role table

--- backend/app/Models/Permission.php
+++ backend/app/Models/Permission.php
@@ -11,4 +11,9 @@ class Permission extends Model
     use HasFactory, SoftDeletes;
 
     protected $guarded = [];
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'permission_role');
+    }
 }
