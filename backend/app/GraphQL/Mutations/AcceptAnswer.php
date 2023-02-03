4aab0873887bef2d958f9ac440348b3e82df3182
IanUrriza10 <114897466+IanUrriza10@users.noreply.github.com>
2023-02-03 13:51:28 +0800

    [Overflow-32] [BE] Create API for Accepting Answer (#47)
    * [Overflow-32] [BE] Create API for Accepting Answer
    
    removed comments
    
    * edits based on comments

---
+++ backend/app/GraphQL/Mutations/AcceptAnswer.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use Exception;
+use App\Models\Answer;
+use App\Models\Question;
+
+final class AcceptAnswer
+{
+    /**
+     * @param  null  $_
+     * @param  array{}  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        // TODO implement the resolver
+        try {
+            $question = Question::findOrFail($args['question_id']);
+
+            $answer = Answer::findOrFail($args['answer_id']);
+            $checkAccepted = $question->answers()->where('answers.is_correct', true)->exists();
+
+            if($checkAccepted){
+                return "Only one answer can be accepted";
+            }
+
+            $answer->is_correct = true;
+            $answer->save();
+
+            return "Answer was accepted successfully";
+
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+}
