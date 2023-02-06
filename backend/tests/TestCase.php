08d376b0d6a198e17434ac77d01eeb6105f70e8a
Keno Bacunawa <kenorenz.bacunawa@sun-asterisk.com>
2023-02-06 11:09:45 +0800

    [OVERFLOW-156] Setup CI / CD WIP

--- backend/tests/TestCase.php
+++ backend/tests/TestCase.php
@@ -2,9 +2,49 @@
 
 namespace Tests;
 
+use Closure;
+use Tests\CreatesApplication;
+use Illuminate\Support\Fluent;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\SQLiteConnection;
+use Illuminate\Database\Schema\SQLiteBuilder;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function __construct(?string $name = null, array $data = [], string $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+        $this->hotfixSqlite();
+    }
+
+    public function hotfixSqlite()
+    {
+        \Illuminate\Database\Connection::resolverFor('sqlite', function ($connection, $database, $prefix, $config) {
+            return new class($connection, $database, $prefix, $config) extends SQLiteConnection
+            {
+                public function getSchemaBuilder()
+                {
+                    if ($this->schemaGrammar === null) {
+                        $this->useDefaultSchemaGrammar();
+                    }
+                    return new class($this) extends SQLiteBuilder
+                    {
+                        protected function createBlueprint($table, Closure $callback = null)
+                        {
+                            return new class($table, $callback) extends Blueprint
+                            {
+                                public function dropForeign($index)
+                                {
+                                    return new Fluent();
+                                }
+                            };
+                        }
+                    };
+                }
+            };
+        });
+    }
 }
