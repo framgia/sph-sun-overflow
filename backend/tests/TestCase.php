9596bd211a9b96bef1d98bf0d3028c30d90eda09
Keno Bacunawa <kenorenz.bacunawa@sun-asterisk.com>
2023-02-06 11:09:45 +0800

    [OVERFLOW-156] Setup CI / CD WIP

--- backend/tests/TestCase.php
+++ backend/tests/TestCase.php
@@ -29,7 +29,6 @@ abstract class TestCase extends BaseTestCase
                     if ($this->schemaGrammar === null) {
                         $this->useDefaultSchemaGrammar();
                     }
-
                     return new class($this) extends SQLiteBuilder
                     {
                         protected function createBlueprint($table, Closure $callback = null)
