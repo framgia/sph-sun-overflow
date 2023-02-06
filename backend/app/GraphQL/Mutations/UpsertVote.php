1d209de3be3134f79747da263f10835a93f02e59
Ian Urriza <ianmichael.urriza@sun-asterisk.com>
2023-02-06 17:11:25 +0800

    [Overflow-30] [BE] Create API for voting
    fixed value condition

---
+++ backend/app/GraphQL/Mutations/UpsertVote.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+use App\Models\Answer;
+use App\Models\Question;
+use Exception;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+final class UpsertVote
+{
+    /**
+     * @param  null  $_
+     * @param  array{}  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        try {
+            if($args['voteable_type'] == "Answer") {
+                $voteable = Answer::findOrFail($args['voteable_id']);
+            }
+
+            if($args['voteable_type'] == "Question") {
+                $voteable = Question::findOrFail($args['voteable_id']);
+            }
+
+            if($args['value'] !== 1 and $args['value'] !== -1){
+                return "Please enter a valid value";
+            }
+
+            $voteable->votes()->updateOrCreate(
+                ['user_id'=> Auth::id()],
+                ['value'=>$args['value']]
+            );
+
+            return "Voted Successfully";
+
+        } catch(Exception $e) {
+            $message = $e->getMessage();
+            if(Str::contains($message, "No query results for model")){
+                return "Invalid voteable_id";
+            }
+        }
+    }
+}
