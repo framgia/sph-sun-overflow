b45ec1d02c11e5ad59fcde138e71dc273f8e2271
Keno Bacunawa <kenorenz.bacunawa@sun-asterisk.com>
2023-02-17 14:59:46 +0800

    [OVERFLOW-57] Create update profile api

---
+++ backend/app/GraphQL/Mutations/UpdateUser.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\URL;
+
+final class UpdateUser
+{
+    /**
+     * @param  null  $_
+     * @param  array{}  $args
+     */
+    public function uploadAvatar($avatar, $email, $path = 'avatar')
+    {
+        if (! $avatar) {
+            return null;
+        }
+        [, $avatar] = explode(',', $avatar);
+        $data = base64_decode($avatar);
+        $fileName = $email.'.png';
+        $storagePath = storage_path("app/public/$path/");
+
+        if (! file_exists($storagePath)) {
+            mkdir($storagePath, 0777, true);
+        }
+
+        file_put_contents($storagePath.$fileName, $data);
+
+        return URL::to('/').'/storage/avatar/'.$fileName;
+    }
+
+    public function __invoke($_, array $args)
+    {
+        $user = Auth::user();
+
+        $user->first_name = $args['first_name'];
+        $user->last_name = $args['last_name'];
+        $user->about_me = $args['about_me'];
+
+        if (str_contains($args['avatar'], 'base64')) {
+            $user->avatar = $this->uploadAvatar($args['avatar'], $user->email);
+        }
+
+        $user->save();
+
+        return $user->fresh();
+    }
+}
