421d5a6caed332df053287cdc607a87321989b8b
kenorenzsun <116168014+kenorenzsun@users.noreply.github.com>
2023-02-17 16:12:21 +0800

    [OVERFLOW-16] Create a page for Search Results (#100)

--- frontend/pages/questions/index.tsx
+++ frontend/pages/questions/index.tsx
@@ -3,13 +3,14 @@ import Filters from '@/components/molecules/Filters'
 import Paginate from '@/components/organisms/Paginate'
 import QuestionList from '@/components/organisms/QuestionList'
 import { useRouter } from 'next/router'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import GET_QUESTIONS from '@/helpers/graphql/queries/get_questions'
 import { useQuery } from '@apollo/client'
 import { loadingScreenShow } from '../../helpers/loaderSpinnerHelper'
 import { errorNotify } from '../../helpers/toast'
 import { QuestionType } from './[slug]'
 import SortDropdown from '@/components/molecules/SortDropdown'
+import { HiSearch } from 'react-icons/hi'
 
 export interface PaginatorInfo {
     currentPage: number
@@ -27,6 +28,8 @@ const QuestionsPage = () => {
     const [selectedDateFilter, setSelectedDateFilter] = useState('Newest first')
     const [selectedAnswerFilter, setSelectedAnswerFilter] = useState('Answered')
     const router = useRouter()
+    const pathIsSearchResult = router.asPath.includes('/questions?search=')
+    const [searchKey, setSearchKey] = useState(router.query.search)
 
     const { data, loading, error, refetch } = useQuery(GET_QUESTIONS, {
         variables: {
@@ -100,8 +103,21 @@ const QuestionsPage = () => {
         },
     ]
 
-    return (
-        <div className="flex h-full w-full flex-col gap-4 px-10 pt-8 pb-5">
+    const renderSortAndFilter = (): JSX.Element => {
+        return (
+            <div className="flex gap-2">
+                <div className="w-32">
+                    <SortDropdown filters={dateFilters} selectedFilter={selectedDateFilter} />
+                </div>
+                <div className="w-32">
+                    <SortDropdown filters={answerFilters} selectedFilter={selectedAnswerFilter} />
+                </div>
+            </div>
+        )
+    }
+
+    const renderQuestionListHeader = (): JSX.Element => {
+        return (
             <div className="flex flex-row items-center justify-between px-5">
                 <Button
                     usage="ask_question"
@@ -111,18 +127,62 @@ const QuestionsPage = () => {
                 >
                     Ask a Question
                 </Button>
-                <div className="flex gap-2">
-                    <div className="w-32">
-                        <SortDropdown filters={dateFilters} selectedFilter={selectedDateFilter} />
-                    </div>
-                    <div className="w-32">
-                        <SortDropdown
-                            filters={answerFilters}
-                            selectedFilter={selectedAnswerFilter}
-                        />
-                    </div>
-                </div>
+                {renderSortAndFilter()}
             </div>
+        )
+    }
+
+    const handleSearchSubmit = (e): void => {
+        e.preventDefault()
+
+        router.push(
+            {
+                pathname: `/questions`,
+                query: {
+                    searchKey,
+                },
+            },
+            `/questions?search=${searchKey}`,
+            { shallow: true }
+        )
+    }
+
+    const renderSearchResultHeader = (): JSX.Element => {
+        return (
+            <div>
+                <div className="w-full border-b-2">
+                    <div className=" pb-5 text-3xl font-semibold">Search Results</div>
+                </div>
+                <div className="mt-2 flex flex-row items-center justify-between px-2">
+                    <form onSubmit={handleSearchSubmit}>
+                        <div className="relative w-48 lg:w-80">
+                            <input
+                                type="text"
+                                name="search"
+                                value={searchKey}
+                                onChange={(e) => setSearchKey(e.target.value)}
+                                className="form-input h-11 w-48 rounded-md border border-gray-300 px-4 text-gray-900 focus:border-red-500 focus:ring-red-500 lg:w-80"
+                                placeholder="Search"
+                                required
+                            />
+                            <Button type="submit" usage="icon">
+                                <HiSearch
+                                    size={20}
+                                    className="mr-1 text-gray-400 hover:text-red-500"
+                                />
+                            </Button>
+                        </div>
+                    </form>
+                    {renderSortAndFilter()}
+                </div>
+                <div className="text-md mt-1 pl-3 text-gray-800">500 result for {searchKey}</div>
+            </div>
+        )
+    }
+
+    return (
+        <div className="flex h-full w-full flex-col gap-4 px-10 pt-8 pb-5">
+            {pathIsSearchResult ? renderSearchResultHeader() : renderQuestionListHeader()}
             <div className="flex h-full flex-col justify-between">
                 <div className="flex flex-col gap-3 divide-y-2 divide-primary-gray">
                     {questionList.map(function (question) {
