436625ab37eece46185cf328c217331a4827c824
IanUrriza10 <114897466+IanUrriza10@users.noreply.github.com>
2023-02-17 10:29:53 +0800

    [Overflow-41] Create API for retrieving tags (search and filter) (#98)
    * [Overflow-41] Create API for retrieving tags (search and filter)
    
    * Fixed linter errors
    
    * Added guard to tag api

---
+++ backend/app/GraphQL/Queries/Tags.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\GraphQL\Queries;
+
+use App\Models\Tag;
+
+final class Tags
+{
+    /**
+     * @param  null  $_
+     * @param  array{}  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        if ($args['column'] === 'questions_count') {
+            return Tag::withCount('questions')->orderBy('questions_count', $args['order']);
+        }
+
+        if ($args['column'] === 'watching_count') {
+            return Tag::withCount('usersWatching')->orderBy('users_watching_count', $args['order']);
+        }
+
+        return Tag::query()->orderby($args['column'], $args['order']);
+    }
+}
