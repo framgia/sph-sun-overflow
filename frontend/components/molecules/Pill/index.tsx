48dc8683a07559c8c78eb55d73c942ea02df3a29
Kent Telebanco <ktelebanco@gmail.com>
2023-02-20 18:11:40 +0800

    [Overflow-237] feature/create-tooltip

--- frontend/components/molecules/Pill/index.tsx
+++ frontend/components/molecules/Pill/index.tsx
@@ -1,4 +1,9 @@
 import Icons from '@/components/atoms/Icons'
+import { Popover } from '@headlessui/react'
+import Tooltips from '../Tooltip'
+import React, { useState } from 'react'
+import { usePopper } from 'react-popper'
+import { Float } from '@headlessui-float/react'
 
 type PillProps = {
     name: string
@@ -6,11 +11,38 @@ type PillProps = {
 }
 
 const Pill = ({ name, is_tag }: PillProps): JSX.Element => {
+    const [referenceElement, setReferenceElement] = useState<HTMLDivElement | null>()
+    const [popperElement, setPopperElement] = useState<HTMLDivElement | null>()
+    const [arrowElement, setArrowElement] = useState<HTMLDivElement | null>()
+    const { styles, attributes } = usePopper(referenceElement, popperElement, {
+        modifiers: [{ name: 'arrow', options: { element: arrowElement } }],
+    })
+
     return (
-        <div className="flex min-w-fit flex-row items-center gap-1 rounded-full bg-red-300 py-1 px-3 text-xs font-normal">
-            {is_tag ? <Icons name="pill_eye" /> : ''}
-            <span>{name}</span>
-        </div>
+        <Popover>
+            <Float placement="bottom-start" offset={5} arrow>
+                <Popover.Button type="button">
+                    <div
+                        className="flex min-w-fit flex-row items-center gap-1 rounded-full bg-red-300 py-1 px-3 text-xs font-normal"
+                        ref={setReferenceElement}
+                    >
+                        {is_tag ? <Icons name="pill_eye" /> : ''}
+                        <span>{name}</span>
+                    </div>
+                </Popover.Button>
+                <Popover.Panel
+                    className="absolute z-10 mt-3 w-96 rounded-lg bg-zinc-200 shadow-lg"
+                    ref={setPopperElement}
+                    style={styles.popper}
+                    {...attributes.popper}
+                >
+                    <div ref={setArrowElement} style={styles.arrow}>
+                        <Float.Arrow className="relative ml-3 h-6 w-6 rotate-45 bg-zinc-200" />
+                    </div>
+                    <Tooltips />
+                </Popover.Panel>
+            </Float>
+        </Popover>
     )
 }
 
--- frontend/components/molecules/Tags/index.tsx
+++ frontend/components/molecules/Tags/index.tsx
@@ -1,42 +1,16 @@
 import Pill from '../Pill'
-import { Popover } from '@headlessui/react'
-import Tooltips from '../Tooltip'
-import { useState } from 'react'
-import { usePopper } from 'react-popper'
 
 type TagsProps = {
     values: { id: number; name: string; is_watched_by_user: boolean }[]
 }
 
 const Tags = ({ values }: TagsProps): JSX.Element => {
-    const [referenceElement, setReferenceElement] = useState<HTMLElement | null>(null)
-    const [popperElement, setPopperElement] = useState<HTMLDivElement | null>(null)
-    const { styles, attributes } = usePopper(referenceElement, popperElement)
-
     return (
-        <Popover className="relative">
-            <Popover.Button ref={setReferenceElement}>
-                <div className="flex w-full justify-start gap-2">
-                    {values.map((value) => {
-                        return (
-                            <Pill
-                                key={value.id}
-                                name={value.name}
-                                is_tag={value.is_watched_by_user}
-                            />
-                        )
-                    })}
-                </div>
-            </Popover.Button>
-            <Popover.Panel
-                className="sm:max-w-s absolute z-10 mt-3 max-w-sm rounded-lg bg-zinc-100 px-4 shadow-lg sm:px-0"
-                ref={setPopperElement}
-                style={styles.popper}
-                {...attributes.popper}
-            >
-                <Tooltips></Tooltips>
-            </Popover.Panel>
-        </Popover>
+        <div className="flex w-full justify-start gap-2">
+            {values.map((value) => {
+                return <Pill key={value.id} name={value.name} is_tag={value.is_watched_by_user} />
+            })}
+        </div>
     )
 }
 
