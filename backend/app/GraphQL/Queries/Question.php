ba2e9f6b06902de2c9175c3758aa63e5e7f049d1
kenorenzsun <116168014+kenorenzsun@users.noreply.github.com>
2023-02-22 18:16:30 +0800

    [OVERFLOW-266] Update question detail API to add sorting of votes and created_at (#126)

--- backend/app/GraphQL/Queries/Question.php
+++ backend/app/GraphQL/Queries/Question.php
@@ -3,6 +3,7 @@
 namespace App\GraphQL\Queries;
 
 use App\Models\Question as ModelsQuestion;
+use Illuminate\Support\Facades\DB;
 
 final class Question
 {
@@ -12,12 +13,28 @@ final class Question
      */
     public function __invoke($_, array $args)
     {
-        $question = ModelsQuestion::where('slug', $args['slug'])->first();
+        $questionQuery = ModelsQuestion::query();
+
+        if (isset($args['answerSort'])) {
+            $questionQuery->with(['answers' => function ($query) use ($args) {
+                foreach ($args['answerSort'] as $sort) {
+                    if ($sort['column'] == 'votes_sum_value') {
+                        $query->withSum(['votes' => function ($query) {
+                            $query->select(DB::raw('COALESCE(SUM(value), 0)'));
+                        }], 'value');
+                    }
+
+                    $query->orderBy($sort['column'], $sort['order']);
+                }
+            }]);
+        }
+
+        $question = $questionQuery->where('slug', $args['slug'])->first();
 
         if ($args['shouldAddViewCount']) {
             $question->update(['views_count' => $question->views_count + 1]);
         }
 
-        return $question->fresh();
+        return $question;
     }
 }
