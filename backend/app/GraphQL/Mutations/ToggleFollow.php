fa47f732ae0089b9fa230738391891208bf97701
kenttelebanco <49836629+kenttelebanco@users.noreply.github.com>
2023-02-23 14:34:54 +0800

    [Overflow-58] [BE] Create follows api in profiles (#124)
    * [Overflow-58] feature/create-follows-api
    
    * [Overflow-58] Create Follows Api
    
    * [Overflow-58] feature/create-follows-api
    
    * Updated Follows API
    
    ---------
    
    Co-authored-by: nicoleamber <nicoleamber.mariscal@sun-asterisk.com>

---
+++ backend/app/GraphQL/Mutations/ToggleFollow.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use App\Models\User;
+use App\Models\UserRelation;
+use Exception;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+
+final class ToggleFollow
+{
+    /**
+     * @param  null  $_
+     * @param  array{}  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        try {
+            $user = User::findOrFail($args['user_id']);
+
+            if (Auth::id() == $user->id) {
+                return 'Invalid. User Cant Follow Themselves';
+            }
+
+            if (! in_array($user->id, Auth::user()->followingIds)) {
+                UserRelation::create([
+                    'follower_id' => Auth::id(),
+                    'following_id' => $user->id,
+                ]);
+
+                return "You are now following {$user->first_name} {$user->last_name}";
+            } else {
+                UserRelation::where([
+                    'follower_id' => Auth::id(),
+                    'following_id' => $user->id,
+                ])->delete();
+
+                return "You unfollowed {$user->first_name} {$user->last_name}";
+            }
+        } catch (Exception $e) {
+            $message = $e->getMessage();
+            if (Str::contains($message, 'No query results for model')) {
+                return 'User does not exist';
+            } else {
+                return 'An error has occurred';
+            }
+        }
+    }
+}
