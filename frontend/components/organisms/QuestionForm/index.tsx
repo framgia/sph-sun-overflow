2550ef71d8b2a3918680ceb44a6d7671b4a89d16
Ian Urriza <ianmichael.urriza@sun-asterisk.com>
2023-03-01 18:17:39 +0800

    removed unused imports

--- frontend/components/organisms/QuestionForm/index.tsx
+++ frontend/components/organisms/QuestionForm/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import TagsInput, { ITag } from '../../molecules/TagsInput'
 import QuestionFormSchema from './schema'
 import { useForm, Controller } from 'react-hook-form'
