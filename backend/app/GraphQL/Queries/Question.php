e90762a41754bb1bb00a5b6edd14cea83570d73a
nicoleamber <nicoleamber.mariscal@sun-asterisk.com>
2023-03-01 18:15:00 +0800

    Updated error message

--- backend/app/GraphQL/Queries/Question.php
+++ backend/app/GraphQL/Queries/Question.php
@@ -2,10 +2,13 @@
 
 namespace App\GraphQL\Queries;
 
+use App\Exceptions\CustomException;
 use App\Models\Question as ModelsQuestion;
 use App\Models\Team;
+use Exception;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 final class Question
 {
@@ -33,21 +36,26 @@ final class Question
 
         $question = $questionQuery->where('slug', $args['slug'])->first();
 
-        $checker = true;
-
-        if (! $question->is_public) {
-            $team = Team::findOrFail($question->team_id);
-            if ($team->members->where('user_id', Auth::id())->isEmpty()) {
-                $checker = false;
+        try {
+            if (! $question->is_public) {
+                $team = Team::findOrFail($question->team_id);
+                if ($team->members->where('user_id', Auth::id())->isEmpty()) {
+                    return new CustomException('Unauthorized', 'You are not allowed to view this question.');
+                }
             }
-        }
 
-        if ($checker) {
             if ($args['shouldAddViewCount']) {
                 $question->update(['views_count' => $question->views_count + 1]);
             }
 
             return $question;
+        } catch (Exception $e) {
+            $message = $e->getMessage();
+            if (Str::contains($message, 'No query results for model')) {
+                return new CustomException('Invalid team_id', 'Team does not exist.');
+            } else {
+                return new CustomException('Error', 'An error has occured.');
+            }
         }
     }
 }
