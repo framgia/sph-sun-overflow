4d46b111cd7804705529b6906df415925196741a
Keno Bacunawa <kenorenz.bacunawa@sun-asterisk.com>
2023-03-01 14:34:00 +0800

    Added Resolvers for Add , Edit, Delete member

---
+++ backend/app/GraphQL/Mutations/UpdateMember.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use App\Exceptions\CustomException;
+use App\Models\Member;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Auth;
+
+final class UpdateMember
+{
+    /**
+     * @param  null  $_
+     * @param  array{}  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        $authId = Auth::id();
+        $member = Member::find($args['id']);
+        $updateData = Arr::except($args, ['tags']);
+
+        if ($authId != $member->team->teamLeader->id) {
+            throw new CustomException('You are not allowed to add members to this team');
+        }
+
+        $member->update($updateData);
+
+        return $member->fresh();
+    }
+}
