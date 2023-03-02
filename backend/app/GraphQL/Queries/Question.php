a055610370a9dbc62ac7668190e62737c14f3906
Nicole Amber Mariscal <116238730+nicoleamber@users.noreply.github.com>
2023-03-02 14:49:26 +0800

    [Overflow-275] Update Question Detail API (#143)
    * [Overflow-275] Update Question Detail API
    
    * Updated error message

--- backend/app/GraphQL/Queries/Question.php
+++ backend/app/GraphQL/Queries/Question.php
@@ -2,8 +2,13 @@
 
 namespace App\GraphQL\Queries;
 
+use App\Exceptions\CustomException;
 use App\Models\Question as ModelsQuestion;
+use App\Models\Team;
+use Exception;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 final class Question
 {
@@ -31,10 +36,26 @@ final class Question
 
         $question = $questionQuery->where('slug', $args['slug'])->first();
 
-        if ($args['shouldAddViewCount']) {
-            $question->update(['views_count' => $question->views_count + 1]);
-        }
+        try {
+            if (! $question->is_public) {
+                $team = Team::findOrFail($question->team_id);
+                if ($team->members->where('user_id', Auth::id())->isEmpty()) {
+                    return new CustomException('Unauthorized', 'You are not allowed to view this question.');
+                }
+            }
 
-        return $question;
+            if ($args['shouldAddViewCount']) {
+                $question->update(['views_count' => $question->views_count + 1]);
+            }
+
+            return $question;
+        } catch (Exception $e) {
+            $message = $e->getMessage();
+            if (Str::contains($message, 'No query results for model')) {
+                return new CustomException('Invalid team_id', 'Team does not exist.');
+            } else {
+                return new CustomException('Error', 'An error has occured.');
+            }
+        }
     }
 }
