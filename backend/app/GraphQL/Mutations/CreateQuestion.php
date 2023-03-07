f2b772f90548b2bd90eff268d8b7d857d7202f49
nicoleamber <nicoleamber.mariscal@sun-asterisk.com>
2023-03-07 13:36:53 +0800

    [Overflow-278] Update Questions API

--- backend/app/GraphQL/Mutations/CreateQuestion.php
+++ backend/app/GraphQL/Mutations/CreateQuestion.php
@@ -16,9 +16,9 @@ final class CreateQuestion
     {
         $questionData = Arr::except($args, ['tags']);
 
-        $hasNoTeam = !isset($args['team_id']) || !$args['team_id'];
+        $hasNoTeam = ! isset($args['team_id']) || ! $args['team_id'];
 
-        if ($hasNoTeam && (isset($args['is_public']) && !$args['is_public'])) {
+        if ($hasNoTeam && (isset($args['is_public']) && ! $args['is_public'])) {
             throw new CustomException('400', 'Public is required when no team is selected');
         }
 
--- backend/app/GraphQL/Queries/TeamMembers.php
+++ backend/app/GraphQL/Queries/TeamMembers.php
@@ -4,7 +4,6 @@ namespace App\GraphQL\Queries;
 
 use App\Exceptions\CustomException;
 use App\Models\Team;
-use Exception;
 use Illuminate\Support\Facades\Auth;
 
 final class TeamMembers
@@ -15,14 +14,15 @@ final class TeamMembers
      */
     public function __invoke($_, array $args)
     {
-            $user_id = Auth::id();
-            $team =  Team::where('slug', $args['team_slug'])->first();
-        if (!$team) {
+        $user_id = Auth::id();
+        $team = Team::where('slug', $args['team_slug'])->first();
+        if (! $team) {
             throw new CustomException('No team found', 'No team with the specified slug found!');
         }
         if ($team->user_id != $user_id) {
             throw new CustomException('Invalid user', 'Please access as the Team Lead');
         }
-            return $team->members();
+
+        return $team->members();
     }
 }
