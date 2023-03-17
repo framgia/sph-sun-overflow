ab4a0172dc728d7f754263309d8f49dff6c13452
jules-lucero <111732984+jules-lucero@users.noreply.github.com>
2023-03-17 14:56:13 +0800

    [Overflow-74] [MARKUP] Implement deletion modals (#195)

--- frontend/components/organisms/DeleteRole/index.tsx
+++ frontend/components/organisms/DeleteRole/index.tsx
@@ -1,12 +1,34 @@
 import Button from '@/components/atoms/Button'
 import Icons from '@/components/atoms/Icons'
+import Modal from '@/components/templates/Modal'
+import { useState } from 'react'
 
 const DeleteRole = (): JSX.Element => {
+    const [showModal, setShowModal] = useState(false)
+
     return (
         <div>
-            <Button usage="toggle-modal">
+            <Button
+                usage="toggle-modal"
+                onClick={() => {
+                    setShowModal(true)
+                }}
+            >
                 <Icons name="table_delete" additionalClass="fill-gray-500" />
             </Button>
+            <Modal
+                title="Delete Role"
+                isOpen={showModal}
+                handleClose={() => {
+                    setShowModal(false)
+                }}
+                handleSubmit={() => {
+                    setShowModal(false)
+                }}
+                submitLabel="Delete"
+            >
+                Are you sure you wish to delete this role?
+            </Modal>
         </div>
     )
 }
