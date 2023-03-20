7dd73bf8be7826ca0ce55435607cd4a441e43a11
Jhon Rhon Escabillas <114911074+jescabillas@users.noreply.github.com>
2023-03-20 14:19:59 +0800

    [Overflow-101] [BE] Create API for deleting tags (#202)
    * [Overflow-101] [BE] Create API for deleting tags
    
    * Code sniffer fixes

---
+++ backend/app/GraphQL/Mutations/DeleteTag.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use App\Exceptions\CustomException;
+use App\Models\Tag;
+
+final class DeleteTag
+{
+    /**
+     * @param  null  $_
+     * @param  array{}  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        $tag = Tag::find($args['id']);
+
+        if (!$tag) {
+            throw new CustomException('Tag not found!');
+        }
+
+        $tag->delete();
+
+        return $tag;
+    }
+}
