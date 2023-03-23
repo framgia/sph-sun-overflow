45dd3f5293a958cbae67b6891d11b013c059d8db
Nicole Amber Mariscal <116238730+nicoleamber@users.noreply.github.com>
2023-03-23 13:16:57 +0800

    [Overflow-95] [MARKUP] Implement Deletion Modal for Tags (#220)

--- frontend/components/organisms/TagsAction/index.tsx
+++ frontend/components/organisms/TagsAction/index.tsx
@@ -1,5 +1,6 @@
 import Button from '@/components/atoms/Button'
 import Icons from '@/components/atoms/Icons'
+import Modal from '@/components/templates/Modal'
 import { useState } from 'react'
 import TagsFormModal from '../TagsFormModal'
 
@@ -11,15 +12,21 @@ type Props = {
 }
 
 const TagsActions = ({ id, name, description, refetchHandler }: Props): JSX.Element => {
-    const [isOpen, setIsOpen] = useState(false)
+    const [showEditModal, setShowEditModal] = useState(false)
+    const [showDeleteModal, setShowDeleteModal] = useState(false)
+
     const initialData = {
         id,
         name,
         description,
     }
 
-    const closeModal = (): void => {
-        setIsOpen(false)
+    const closeEditModal = (): void => {
+        setShowEditModal(false)
+    }
+
+    const closeDeleteModal = (): void => {
+        setShowDeleteModal(false)
     }
 
     return (
@@ -27,20 +34,37 @@ const TagsActions = ({ id, name, description, refetchHandler }: Props): JSX.Elem
             <Button
                 usage="toggle-modal"
                 onClick={(): void => {
-                    setIsOpen(true)
+                    setShowEditModal(true)
                 }}
             >
                 <Icons name="table_edit" additionalClass="fill-gray-500" />
             </Button>
-            <Button usage="toggle-modal">
+            <Button
+                usage="toggle-modal"
+                onClick={(): void => {
+                    setShowDeleteModal(true)
+                }}
+            >
                 <Icons name="table_delete" additionalClass="fill-gray-500" />
             </Button>
             <TagsFormModal
-                isOpen={isOpen}
-                closeModal={closeModal}
+                isOpen={showEditModal}
+                closeModal={closeEditModal}
                 refetchHandler={refetchHandler}
                 initialData={initialData}
             />
+            <Modal
+                title="Delete Tag"
+                isOpen={showDeleteModal}
+                handleClose={closeDeleteModal}
+                handleSubmit={closeDeleteModal}
+                submitLabel="Delete"
+            >
+                <span>
+                    Are you sure you wish to delete the{' '}
+                    <span className="font-bold">JavaScript</span> tag?
+                </span>
+            </Modal>
         </div>
     )
 }
