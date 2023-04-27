36c7e9793b2a2731724f793d8e926ee9a5db8cbc
Ian Urriza <ianmichael.urriza@sun-asterisk.com>
2023-04-27 10:29:18 +0800

    [Overflow-479] [Bugfix] TagsInput having 6 tags

--- frontend/components/organisms/QuestionForm/schema.tsx
+++ frontend/components/organisms/QuestionForm/schema.tsx
@@ -17,7 +17,8 @@ const QuestionFormSchema = yup.object().shape({
                 title: yup.string(),
             })
         )
-        .min(1, 'Questions must have at least 1 tag'),
+        .min(1, 'Questions must have at least 1 tag')
+        .max(5, 'Questions can have a max of 5 tags'),
 })
 
 export default QuestionFormSchema
