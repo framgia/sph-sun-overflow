fb2250415e2323fdfc8ba4047d46b3cf60ca9652
nicoleamber <nicoleamber.mariscal@sun-asterisk.com>
2023-04-28 10:13:41 +0800

    [Overflow-458] Refactor Users Page

--- frontend/components/molecules/UserCard/index.tsx
+++ frontend/components/molecules/UserCard/index.tsx
@@ -8,28 +8,34 @@ type UserProps = {
 
 const UserCard = ({ user }: UserProps): JSX.Element => {
     return (
-        <Link href={`/users/${user.slug}`} title={`${user.first_name} ${user.last_name}`}>
-            <div className="min-w-sm max-w-md rounded-md border border-gray-300 bg-white hover:shadow-xl">
-                <div className="flex flex-row items-center justify-start p-4">
-                    <div className="px-4">
-                        <Avatar
-                            round={true}
-                            name={`${user.first_name} ${user.last_name}`}
-                            size="60"
-                            alt={user.first_name}
-                            src={user.avatar}
-                            maxInitials={1}
-                            textSizeRatio={2}
-                            className={`flex-0 aspect-square bg-gray-800 text-sm`}
-                        />
-                    </div>
-                    <div className="flex max-w-fit flex-col">
-                        <span className="text-md font-medium leading-tight text-gray-800 line-clamp-1">
-                            {`${user.first_name} ${user.last_name}`}
-                        </span>
-                        <span className="text-sm text-gray-500">{user.role.name}</span>
-                        <span className="text-xs text-gray-800">{user.reputation} pts</span>
-                    </div>
+        <Link
+            href={`/users/${user.slug}`}
+            title={`${user.first_name} ${user.last_name}`}
+            className="min-w-sm max-w-md rounded-[5px] border border-neutral-disabled bg-white"
+        >
+            <div className="flex flex-row items-center justify-start gap-2 p-4">
+                <div>
+                    <Avatar
+                        round={true}
+                        name={`${user.first_name} ${user.last_name}`}
+                        size="56"
+                        alt={user.first_name}
+                        src={user.avatar}
+                        maxInitials={1}
+                        textSizeRatio={2}
+                        className={`flex-0 aspect-square border border-[#3D3D3D] text-sm`}
+                    />
+                </div>
+                <div className="flex h-[53px] w-full flex-col gap-1">
+                    <span className="text-left text-sm font-medium leading-[120%] text-neutral-900 line-clamp-1">
+                        {`${user.first_name} ${user.last_name}`}
+                    </span>
+                    <span className="text-left text-xs font-normal text-neutral-disabled line-clamp-1">
+                        {user.role.name}
+                    </span>
+                    <span className="text-left text-xs font-normal text-neutral-900 line-clamp-1">
+                        {user.reputation} points
+                    </span>
                 </div>
             </div>
         </Link>
