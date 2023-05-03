e6d9ea5bb4408d52cd22b4b44270da31f16e0c89
Ian Urriza <ianmichael.urriza@sun-asterisk.com>
2023-05-03 17:12:35 +0800

    added minimum height to cards

--- frontend/pages/teams/index.tsx
+++ frontend/pages/teams/index.tsx
@@ -74,65 +74,67 @@ const TeamsListPage = (): JSX.Element => {
     }
 
     return (
-        <div className="h-full w-full overflow-y-auto rounded-[5px] border border-neutral-200 bg-neutral-white p-4">
-            <div className="flex w-full justify-between">
-                <div className="text-xl font-semibold text-neutral-900">My Teams</div>
-            </div>
-            <div className="mt-4 w-full">
-                <div>
-                    <form onSubmit={handleSearchSubmit}>
-                        <InputField
-                            name="search"
-                            placeholder="Search"
-                            icon={
-                                <div className="absolute top-1/2 left-1.5 -translate-y-1/2 transform">
-                                    <Search />
-                                </div>
-                            }
-                            additionalClass="h-10 w-72 pl-8"
-                            value={searchKey}
-                            onChange={(e) => {
-                                onSearchInputChange(e.target.value)
-                            }}
-                        />
-                    </form>
-                    {term && (
-                        <div className="w-80">
-                            <div className="truncate px-2 pt-1 text-sm text-gray-600">
-                                {`${pageInfo?.total} search ${
-                                    pageInfo?.total !== 1 ? `results` : `result`
-                                } for "${term}"`}
-                            </div>
-                        </div>
-                    )}
+        <div className="flex w-full justify-center rounded-[5px] border border-neutral-200 bg-neutral-white p-4">
+            <div className="flex w-[886px] flex-col gap-4 align-middle">
+                <div className="flex w-full justify-between">
+                    <div className="text-xl font-semibold text-neutral-900">My Teams</div>
                 </div>
-            </div>
-            {teams?.length !== 0 ? (
-                <div>
-                    <div className="flex flex-wrap gap-4 pt-4 ">
-                        {teams?.map((team) => {
-                            return (
-                                <TeamCard
-                                    key={team.id}
-                                    slug={team.slug ?? ''}
-                                    name={team.name}
-                                    description={team.description}
-                                    usersCount={team.members_count}
-                                />
-                            )
-                        })}
-                    </div>
-                    <div className=" mt-4 flex h-14 w-full items-center justify-center">
-                        {pageInfo?.lastPage > 1 && (
-                            <Paginate {...pageInfo} onPageChange={onPageChange} />
+                <div className="w-full">
+                    <div>
+                        <form onSubmit={handleSearchSubmit}>
+                            <InputField
+                                name="search"
+                                placeholder="Search"
+                                icon={
+                                    <div className="absolute top-1/2 left-1.5 -translate-y-1/2 transform">
+                                        <Search />
+                                    </div>
+                                }
+                                additionalClass="h-10 w-72 pl-8"
+                                value={searchKey}
+                                onChange={(e) => {
+                                    onSearchInputChange(e.target.value)
+                                }}
+                            />
+                        </form>
+                        {term && (
+                            <div className="w-80">
+                                <div className="truncate px-2 pt-1 text-sm text-gray-600">
+                                    {`${pageInfo?.total} search ${
+                                        pageInfo?.total !== 1 ? `results` : `result`
+                                    } for "${term}"`}
+                                </div>
+                            </div>
                         )}
                     </div>
                 </div>
-            ) : (
-                <span className="mt-4 flex h-20 w-full items-center justify-center p-2 text-center text-lg font-bold text-primary-gray">
-                    No teams to show
-                </span>
-            )}
+                {teams?.length !== 0 ? (
+                    <div className="">
+                        <div className="flex min-h-[632px] flex-wrap gap-4">
+                            {teams?.map((team) => {
+                                return (
+                                    <TeamCard
+                                        key={team.id}
+                                        slug={team.slug ?? ''}
+                                        name={team.name}
+                                        description={team.description}
+                                        usersCount={team.members_count}
+                                    />
+                                )
+                            })}
+                        </div>
+                        <div className=" mt-4 flex h-14 w-full items-center justify-center">
+                            {pageInfo?.lastPage > 1 && (
+                                <Paginate {...pageInfo} onPageChange={onPageChange} />
+                            )}
+                        </div>
+                    </div>
+                ) : (
+                    <span className="flex h-20 w-full items-center justify-center p-2 text-center text-lg font-bold text-primary-gray">
+                        No teams to show
+                    </span>
+                )}
+            </div>
         </div>
     )
 }
