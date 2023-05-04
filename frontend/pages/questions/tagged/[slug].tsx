f3bd8a23b62c11be471acd1a840e856ca06735f4
jules-lucero <julesrussel.lucero@sun-asterisk.com>
2023-05-04 14:33:24 +0800

    [Overflow-501] [BUGFIX] Capitalize tag and display humanized created at

--- frontend/pages/questions/tagged/[slug].tsx
+++ frontend/pages/questions/tagged/[slug].tsx
@@ -1,18 +1,22 @@
 import QuestionsPageLayout from '@/components/templates/QuestionsPageLayout'
 import GET_QUESTIONS from '@/helpers/graphql/queries/get_questions'
+import GET_TAG from '@/helpers/graphql/queries/get_tag'
 import { useQuery } from '@apollo/client'
 import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { loadingScreenShow } from '../../../helpers/loaderSpinnerHelper'
 import { errorNotify } from '../../../helpers/toast'
 import { answerFilterOption, orderByOptions, type RefetchType } from '../index'
 
-const TagsPage = (): JSX.Element => {
+const TagQuestionsPage = (): JSX.Element => {
     const router = useRouter()
-    const [selectedTag, setSelectedTag] = useState(router.query.slug as string)
+    const selectedTag = String(router.query.slug ?? '')
     const order = orderByOptions[String(router.query.order ?? 'Newest first')]
     const answerFilter = answerFilterOption[String(router.query.filter ?? '')]
-    const { data, loading, error, refetch } = useQuery<any, RefetchType>(GET_QUESTIONS, {
+    const tagQuery = useQuery(GET_TAG, {
+        variables: { slug: selectedTag },
+    })
+    const questionsQuery = useQuery<any, RefetchType>(GET_QUESTIONS, {
         variables: {
             first: 10,
             page: 1,
@@ -22,30 +26,30 @@ const TagsPage = (): JSX.Element => {
     })
 
     useEffect(() => {
-        const { slug } = router.query
-
-        void refetch({
+        void questionsQuery.refetch({
             first: 10,
             page: 1,
             orderBy: [order],
             filter: { tag: selectedTag, ...answerFilter },
         })
-        setSelectedTag(slug as string)
-    }, [router, selectedTag, refetch])
+    }, [router, selectedTag, questionsQuery.refetch])
+
+    const loading = questionsQuery.loading || tagQuery.loading
+    const error = questionsQuery.error ?? tagQuery.error
 
     if (loading) return loadingScreenShow()
     if (error) return <span>{errorNotify(`Error! ${error.message}`)}</span>
 
     return (
         <QuestionsPageLayout
-            refetch={refetch}
-            data={data}
+            refetch={questionsQuery.refetch}
+            data={questionsQuery.data}
             isSearchResult={false}
             isPrivate={false}
             page_slug={'questions'}
-            selectedTag={selectedTag}
+            selectedTag={tagQuery.data.tag.name}
         />
     )
 }
 
-export default TagsPage
+export default TagQuestionsPage
