de53a00cf3c78948281cfa10506972a8eff815ed
jules-lucero <111732984+jules-lucero@users.noreply.github.com>
2023-05-12 14:52:47 +0800

    [Overflow-538] [BUGFIX] Tags Input dropdown margin (#341)

--- frontend/components/molecules/TagsInput/index.tsx
+++ frontend/components/molecules/TagsInput/index.tsx
@@ -124,7 +124,7 @@ const TagsInput = ({
                             setQueryText('')
                         }}
                     >
-                        <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                        <Combobox.Options className="absolute mt-2 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
                             {filteredTags.length === 0 && queryText !== '' ? (
                                 <div className="relative z-10 cursor-default select-none  bg-white px-4 py-2 text-gray-700">
                                     Nothing found.
