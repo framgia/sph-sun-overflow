c3bd3979200f5a661b0e1996b0b756392f65dd9f
jescabillas <jhonrhon.escabillas@sun-asterisk.com>
2023-05-15 15:38:55 +0800

    Background color change

--- frontend/components/molecules/ClickBack/index.tsx
+++ frontend/components/molecules/ClickBack/index.tsx
@@ -11,12 +11,12 @@ const ClickBack = (): JSX.Element => {
     return (
         <div
             onClick={goBack}
-            className="group flex h-10 w-10 cursor-pointer items-center justify-center rounded-full duration-200 hover:bg-primary-200"
+            className="group flex h-10 w-10 cursor-pointer items-center justify-center rounded-full duration-200 hover:bg-primary-500"
         >
             <Icons
                 name="back_arrow"
                 size="24"
-                additionalClass="fill-neutral-disabled group-hover:fill-primary-base duration-200"
+                additionalClass="fill-neutral-disabled group-hover:fill-neutral-white duration-200"
             />
         </div>
     )
